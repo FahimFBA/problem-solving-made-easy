cde7c9df7bda868775d41b5c142ca1cfcb483ab7
FahimFBA <fahimbinamin@gmail.com>
2022-10-13 03:05:43 +0600

    Create solve.cpp

---
+++ P - 2/solve.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int variable;
+    cin >> variable;
+    if (variable % 2 == 0)
+    {
+        cout << "Even Number" << endl;
+    }
+    else
+    {
+        cout << "Odd Number" << endl;
+    }
+}
