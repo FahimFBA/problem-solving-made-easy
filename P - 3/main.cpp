edb5c68725855e58fed295b5cf113d64d8b2e7b1
FahimFBA <fahimbinamin@gmail.com>
2022-10-17 13:37:01 +0600

    problem 3 added

---
+++ P - 3/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int variable;
+    cin >> variable;
+    if (variable % 3 == 0)
+        cout << variable << " is divisible by 3" << endl;
+    else
+        cout << variable << " is not divisible by 3" << endl;
+}
