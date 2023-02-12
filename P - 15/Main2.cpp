05a12926bc1a0327345bfb31d6e045215c4d1e49
FahimFBA <fahimbinamin@gmail.com>
2023-02-12 19:17:23 +0600

    code updated

---
+++ P - 15/Main2.cpp
@@ -0,0 +1,34 @@
+// Print array elements in the given order and reversely
+
+#include <bits/stdc++.h>
+using namespace std;
+void printReversely(int arr[], int arraySize);
+int main()
+{
+    cout << "Enter the array size: ";
+    int arraySize;
+    cin >> arraySize;
+    int arr[arraySize];
+    cout << "Enter the array elements " << endl;
+    for (int i = 0; i < arraySize; i++)
+    {
+        cin >> arr[i];
+    }
+    // printing the array in the correct order
+    cout << "Printing the array in the original order" << endl;
+    for (int i = 0; i < arraySize; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    // printing the array in the reverse order
+    cout << "\nPrinting the array in the reversed order" << endl;
+    printReversely(arr, arraySize);
+}
+
+void printReversely(int arr[], int arraySize)
+{
+    for (int i = arraySize - 1; i >= 0; i--)
+    {
+        cout << arr[i] << " ";
+    }
+}
